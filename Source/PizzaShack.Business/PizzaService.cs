327d09948f9656abb5d7bb505efb05a408ec67e2
Jake Overall <joverall22@gmail.com>
2018-10-29 20:05:22 -0600

    add service layer to controller

---
+++ Source/PizzaShack.Business/PizzaService.cs
@@ -0,0 +1,33 @@
+﻿using PizzaShack.Data;
+using PizzaShack.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaShack.Business
+{
+    public class PizzaService
+    {
+        private readonly PizzaContext _context;
+
+        public PizzaService(PizzaContext context)
+        {
+            _context = context;
+        }
+
+
+        public void CreatePizza(Pizza pizza)
+        {
+            //do some work business stuff
+
+            _context.Pizzas.Add(pizza);
+            _context.SaveChanges();
+        }
+
+        public List<Pizza> GetPizzas()
+        {
+            //NEVER RETURN IQUERYABLE out of BL OR DBSET
+            return _context.Pizzas.ToList();
+        }
+    }
+}
